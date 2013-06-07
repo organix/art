ba033a39d60bf63fad285064fb146ad86af03e53
Dale Schumacher <dalnefre@yahoo.com>
2013-06-07 11:21:00 +0200

    Experimenting with different low-level data structures

--- art.c
+++ art.c
@@ -64,7 +64,6 @@ KIND(object_kind)
 }
 
 struct object undef_object = { object_kind };
-struct object nil_object = { object_kind };
 
 OOP
 object_call(OOP obj, OOP msg)
@@ -72,42 +71,185 @@ object_call(OOP obj, OOP msg)
 	return (obj->kind)(obj, msg);
 }
 
+struct object nil_object = { object_kind };
+
 /*
-message:
-	Messages consists of a selector 'name' and related 'args'.
+pair:
+	A combination of 2 object pointers.
 */
 
-struct message {
+struct pair {
 	struct object	o;
-	OOP				name;
-	OOP				args;
+	OOP				h;
+	OOP				t;
 };
 
-KIND(message_kind);
+KIND(pair_kind);
 
-struct message *
-message_new(OOP name, OOP args)
+struct pair *
+pair_new(OOP h, OOP t)
 {
-	struct message * oop = (struct message *)object_new(message_kind, sizeof(struct message));
-	oop->name = name;
-	oop->args = args;
+	struct pair * oop = (struct pair *)object_new(pair_kind, sizeof(struct pair));
+	oop->h = h;
+	oop->t = t;
 	return oop;
 }
 
 struct symbol name_symbol;
 struct symbol args_symbol;
 
-KIND(message_kind)
+KIND(pair_kind)
 {
-	if (message_kind == self->kind) {
-		struct message * oop = (struct message *)self;		
-		if (message_kind == msg->kind) {
-			struct message * mp = (struct message *)msg;		
-			if (mp->name == (OOP)&name_symbol) {
-				return oop->name;
+	if (pair_kind == self->kind) {
+		struct pair * oop = (struct pair *)self;
+		/* no message protocol for pair */
+	}
+	return &undef_object;
+}
+
+/*
+finger:
+	Small collections of 1 to 4 items.
+*/
+
+struct finger {
+	struct object	o;
+	OOP				item_1;
+	OOP				item_2;
+	OOP				item_3;
+	OOP				item_4;
+};
+
+KIND(finger_1_kind);
+KIND(finger_2_kind);
+KIND(finger_3_kind);
+KIND(finger_4_kind);
+
+struct finger *
+finger_1_new(OOP item_1) {
+	struct finger * oop = (struct finger *)object_new(finger_1_kind, sizeof(struct finger));
+	oop->item_1 = item_1;
+	return oop;
+}
+
+struct finger *
+finger_2_new(OOP item_1, OOP item_2) {
+	struct finger * oop = (struct finger *)object_new(finger_2_kind, sizeof(struct finger));
+	oop->item_1 = item_1;
+	oop->item_2 = item_2;
+	return oop;
+}
+
+struct finger *
+finger_3_new(OOP item_1, OOP item_2, OOP item_3) {
+	struct finger * oop = (struct finger *)object_new(finger_3_kind, sizeof(struct finger));
+	oop->item_1 = item_1;
+	oop->item_2 = item_2;
+	oop->item_3 = item_3;
+	return oop;
+}
+
+struct finger *
+finger_4_new(OOP item_1, OOP item_2, OOP item_3, OOP item_4) {
+	struct finger * oop = (struct finger *)object_new(finger_4_kind, sizeof(struct finger));
+	oop->item_1 = item_1;
+	oop->item_2 = item_2;
+	oop->item_3 = item_3;
+	oop->item_4 = item_4;
+	return oop;
+}
+
+struct symbol put_symbol;
+struct symbol pop_symbol;
+struct symbol push_symbol;
+struct symbol pull_symbol;
+
+KIND(finger_1_kind)
+{
+	if (finger_1_kind == self->kind) {
+		struct finger * oop = (struct finger *)self;
+		if (finger_2_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&put_symbol) {
+				return (OOP)finger_2_new(oop->item_1, mfp->item_2);
 			}
-			if (mp->name == (OOP)&args_symbol) {
-				return oop->args;
+			if (mfp->item_1 == (OOP)&push_symbol) {
+				return (OOP)finger_2_new(mfp->item_2, oop->item_1);
+			}
+		}
+	}
+	return &undef_object;
+}
+
+KIND(finger_2_kind)
+{
+	if (finger_2_kind == self->kind) {
+		struct finger * oop = (struct finger *)self;
+		if (finger_1_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&pop_symbol) {
+				return (OOP)pair_new(oop->item_1, 
+						(OOP)finger_1_new(oop->item_2));
+			}
+			if (mfp->item_1 == (OOP)&pull_symbol) {
+				return (OOP)pair_new(oop->item_2, 
+						(OOP)finger_1_new(oop->item_1));
+			}
+		}
+		if (finger_2_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&put_symbol) {
+				return (OOP)finger_3_new(oop->item_1, oop->item_2, mfp->item_2);
+			}
+			if (mfp->item_1 == (OOP)&push_symbol) {
+				return (OOP)finger_3_new(mfp->item_2, oop->item_1, oop->item_2);
+			}
+		}
+	}
+	return &undef_object;
+}
+
+KIND(finger_3_kind)
+{
+	if (finger_3_kind == self->kind) {
+		struct finger * oop = (struct finger *)self;
+		if (finger_1_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&pop_symbol) {
+				return (OOP)pair_new(oop->item_1, 
+						(OOP)finger_2_new(oop->item_2, oop->item_3));
+			}
+			if (mfp->item_1 == (OOP)&pull_symbol) {
+				return (OOP)pair_new(oop->item_3, 
+						(OOP)finger_2_new(oop->item_1, oop->item_2));
+			}
+		}
+		if (finger_2_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&put_symbol) {
+				return (OOP)finger_4_new(oop->item_1, oop->item_2, oop->item_3, mfp->item_2);
+			}
+			if (mfp->item_1 == (OOP)&push_symbol) {
+				return (OOP)finger_4_new(mfp->item_2, oop->item_1, oop->item_2, oop->item_3);
+			}
+		}
+	}
+	return &undef_object;
+}
+
+KIND(finger_4_kind)
+{
+	if (finger_4_kind == self->kind) {
+		struct finger * oop = (struct finger *)self;
+		if (finger_1_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&pop_symbol) {
+				return (OOP)pair_new(oop->item_1, 
+						(OOP)finger_3_new(oop->item_2, oop->item_3, oop->item_4));
+			}
+			if (mfp->item_1 == (OOP)&pull_symbol) {
+				return (OOP)pair_new(oop->item_4, 
+						(OOP)finger_3_new(oop->item_1, oop->item_2, oop->item_3));
 			}
 		}
 	}
@@ -129,8 +271,10 @@ KIND(symbol_kind);
 struct symbol _t_symbol = { { symbol_kind }, "#t" };
 struct symbol _f_symbol = { { symbol_kind }, "#f" };
 struct symbol eq_symbol = { { symbol_kind }, "eq" };
-struct symbol name_symbol = { { symbol_kind }, "name" };
-struct symbol args_symbol = { { symbol_kind }, "args" };
+struct symbol put_symbol = { { symbol_kind }, "put" };
+struct symbol pop_symbol = { { symbol_kind }, "pop" };
+struct symbol push_symbol = { { symbol_kind }, "push" };
+struct symbol pull_symbol = { { symbol_kind }, "pull" };
 struct symbol bind_symbol = { { symbol_kind }, "bind" };
 struct symbol lookup_symbol = { { symbol_kind }, "lookup" };
 
@@ -146,10 +290,10 @@ KIND(symbol_kind)
 {
 	if (symbol_kind == self->kind) {
 		struct symbol * oop = (struct symbol *)self;
-		if (message_kind == msg->kind) {
-			struct message * mp = (struct message *)msg;		
-			if (mp->name == (OOP)&eq_symbol) {
-				if (self == mp->args) {
+		if (finger_2_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&eq_symbol) {
+				if (mfp->item_2 == self) {
 					return (OOP)&_t_symbol;
 				}
 				return (OOP)&_f_symbol;
@@ -163,7 +307,7 @@ KIND(symbol_kind)
 dict:
 	Dictionaries define mappings from names to values.
 	
-	In this implementation, each node holds a single name/value pair.
+	In this implementation, each node holds a single 'name'/'value' pair.
 	The 'next' pointer delegates to a linear chain of dictionaries.
 */
 
@@ -190,19 +334,19 @@ KIND(dict_kind)
 {
 	if (dict_kind == self->kind) {
 		struct dict * oop = (struct dict *)self;
-		if (message_kind == msg->kind) {
-			struct message * mp = (struct message *)msg;		
-			if (mp->name == (OOP)&bind_symbol) {
-				if (message_kind == mp->args->kind) {
-					mp = (struct message *)mp->args;
-					return (OOP)dict_new(mp->name, mp->args, self);
-				}
-			}
-			if (mp->name == (OOP)&lookup_symbol) {
-				if (mp->args == oop->name) {  // [FIXME] consider using "eq" message here
+		if (finger_2_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&lookup_symbol) {
+				if (mfp->item_2 == oop->name) {  // [FIXME] consider using "eq" message here
 					return oop->value;
 				}
 				return object_call(oop->next, msg);
+			}
+		}
+		if (finger_3_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&bind_symbol) {
+				return (OOP)dict_new(mfp->item_2, mfp->item_3, self);
 			}
 		}
 	}
@@ -212,16 +356,16 @@ KIND(dict_kind)
 KIND(empty_dict_kind)
 {
 	if (empty_dict_kind == self->kind) {
-		if (message_kind == msg->kind) {
-			struct message * mp = (struct message *)msg;		
-			if (mp->name == (OOP)&bind_symbol) {
-				if (message_kind == mp->args->kind) {
-					mp = (struct message *)mp->args;
-					return (OOP)dict_new(mp->name, mp->args, self);
-				}
-			}
-			if (mp->name == (OOP)&lookup_symbol) {
+		if (finger_2_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&lookup_symbol) {
 				return &undef_object;
+			}
+		}
+		if (finger_3_kind == msg->kind) {
+			struct finger * mfp = (struct finger *)msg;
+			if (mfp->item_1 == (OOP)&bind_symbol) {
+				return (OOP)dict_new(mfp->item_2, mfp->item_3, self);
 			}
 		}
 	}
@@ -246,46 +390,49 @@ main()
 	TRACE(fprintf(stderr, "eq_symbol.o.kind = %p\n", (void*)eq_symbol.o.kind));
 	TRACE(fprintf(stderr, "eq_symbol.s = \"%s\"\n", eq_symbol.s));
 
-	struct message * mp = message_new((OOP)&eq_symbol, (OOP)&_f_symbol);
-	TRACE(fprintf(stderr, "mp = %p\n", mp));
-	TRACE(fprintf(stderr, "message_kind = %p\n", (void*)message_kind));
-	TRACE(fprintf(stderr, "mp->o.kind = %p\n", (void*)mp->o.kind));
-	TRACE(fprintf(stderr, "mp->name = %p\n", mp->name));
-	TRACE(fprintf(stderr, "mp->args = %p\n", mp->args));
+	TRACE(fprintf(stderr, "finger_1_kind = %p\n", (void*)finger_1_kind));
+	TRACE(fprintf(stderr, "finger_2_kind = %p\n", (void*)finger_2_kind));
+	TRACE(fprintf(stderr, "finger_3_kind = %p\n", (void*)finger_3_kind));
+	TRACE(fprintf(stderr, "finger_4_kind = %p\n", (void*)finger_4_kind));
 
-	OOP result = object_call((OOP)&_f_symbol, (OOP)mp);
+	struct finger * fp = finger_2_new((OOP)&eq_symbol, (OOP)&_f_symbol);
+	TRACE(fprintf(stderr, "fp = %p\n", fp));
+	TRACE(fprintf(stderr, "finger: %p [%p %p %p %p]\n", fp->o.kind, 
+		fp->item_1, fp->item_2, fp->item_3, fp->item_4));
+	OOP result = object_call((OOP)&_f_symbol, (OOP)fp);
 	TRACE(fprintf(stderr, "result = %p\n", result));
 	assert((OOP)&_t_symbol == result);
 	
 	struct symbol * x_symbol_ptr = symbol_new("x");
 	TRACE(fprintf(stderr, "x_symbol_ptr = %p\n", x_symbol_ptr));
 	TRACE(fprintf(stderr, "x_symbol_ptr->s = \"%s\"\n", x_symbol_ptr->s));
-	mp = message_new((OOP)&lookup_symbol, (OOP)x_symbol_ptr);
-	TRACE(fprintf(stderr, "mp = %p\n", mp));
-	TRACE(fprintf(stderr, "mp->o.kind = %p\n", (void*)mp->o.kind));
-	TRACE(fprintf(stderr, "mp->name = %p\n", mp->name));
-	TRACE(fprintf(stderr, "mp->args = %p\n", mp->args));
-	result = object_call(&empty_dict, (OOP)mp);
+	fp = finger_2_new((OOP)&lookup_symbol, (OOP)x_symbol_ptr);
+	TRACE(fprintf(stderr, "fp = %p\n", fp));
+	TRACE(fprintf(stderr, "finger: %p [%p %p %p %p]\n", fp->o.kind, 
+		fp->item_1, fp->item_2, fp->item_3, fp->item_4));
+	result = object_call(&empty_dict, (OOP)fp);
 	TRACE(fprintf(stderr, "result = %p\n", result));
 	assert(&undef_object == result);
 	
 	struct symbol * _42_symbol_ptr = symbol_new("42");
 	TRACE(fprintf(stderr, "_42_symbol_ptr = %p\n", _42_symbol_ptr));
 	TRACE(fprintf(stderr, "_42_symbol_ptr->s = \"%s\"\n", _42_symbol_ptr->s));
-	mp = message_new((OOP)x_symbol_ptr, (OOP)_42_symbol_ptr);
-	mp = message_new((OOP)&bind_symbol, (OOP)mp);
-	OOP env_ptr = object_call(&empty_dict, (OOP)mp);
+	fp = finger_3_new((OOP)&bind_symbol, (OOP)x_symbol_ptr, (OOP)_42_symbol_ptr);
+	TRACE(fprintf(stderr, "fp = %p\n", fp));
+	TRACE(fprintf(stderr, "finger: %p [%p %p %p %p]\n", fp->o.kind, 
+		fp->item_1, fp->item_2, fp->item_3, fp->item_4));
+	OOP env_ptr = object_call(&empty_dict, (OOP)fp);
 	TRACE(fprintf(stderr, "env_ptr = %p\n", env_ptr));
-	mp = message_new((OOP)&lookup_symbol, (OOP)x_symbol_ptr);
-	result = object_call(env_ptr, (OOP)mp);
+	fp = finger_2_new((OOP)&lookup_symbol, (OOP)x_symbol_ptr);
+	result = object_call(env_ptr, (OOP)fp);
 	TRACE(fprintf(stderr, "result = %p\n", result));
 	assert((OOP)_42_symbol_ptr == result);
 
 	struct symbol * y_symbol_ptr = symbol_new("y");
 	TRACE(fprintf(stderr, "y_symbol_ptr = %p\n", y_symbol_ptr));
 	TRACE(fprintf(stderr, "y_symbol_ptr->s = \"%s\"\n", y_symbol_ptr->s));
-	mp = message_new((OOP)&lookup_symbol, (OOP)y_symbol_ptr);
-	result = object_call(env_ptr, (OOP)mp);
+	fp = finger_2_new((OOP)&lookup_symbol, (OOP)y_symbol_ptr);
+	result = object_call(env_ptr, (OOP)fp);
 	TRACE(fprintf(stderr, "result = %p\n", result));
 	assert(&undef_object == result);
 
